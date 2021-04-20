24240669978cbead7a1568222c837bbba74f6bc6
Alexander Melnikov <minifets@gmail.com>
2021-04-20 21:53:02 +0600

    Install assets

---
+++ public/js/homepage.js
@@ -0,0 +1,16 @@
+var d = new Date('2021-06-04 17:30');
+
+// default example
+simplyCountdown('.simply-countdown-one', {
+    year: d.getFullYear(),
+    month: d.getMonth() + 1,
+    day: d.getDate()
+});
+
+//jQuery example
+$('#simply-countdown-losange').simplyCountdown({
+    year: d.getFullYear(),
+    month: d.getMonth() + 1,
+    day: d.getDate(),
+    enableUtc: false
+});
