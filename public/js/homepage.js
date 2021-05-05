67b2ec3a01354b71728cb2d4f770ba8170a09e8e
Alexander Melnikov <minifets@gmail.com>
2021-05-05 09:19:38 +0600

    Fixes in date.

--- public/js/homepage.js
+++ public/js/homepage.js
@@ -1,17 +1,10 @@
-var d = new Date('2021-06-04T17:30:00+06:00');
-
 // default example
 simplyCountdown('.simply-countdown-one', {
-    year: d.getFullYear(),
-    month: d.getMonth() + 1,
-    day: d.getDate(),
-    plural: true
-});
-
-//jQuery example
-$('#simply-countdown-losange').simplyCountdown({
-    year: d.getFullYear(),
-    month: d.getMonth() + 1,
-    day: d.getDate(),
+    year: 2021,
+    month: 6,
+    day: 4,
+    hours: 17,
+    minutes: 30,
+    plural: true,
     enableUtc: false
 });
